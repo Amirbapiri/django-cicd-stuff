6ac0620c0f4cfe851d3f3397c6cd7585b2bf8ca3
Amir <amircodes10@gmail.com>
2023-05-25 22:29:21 +0330

    create initial model for 'blog' app

--- djangocicd/blog/models.py
+++ djangocicd/blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+from djangocicd.common.models import BaseModel
+
+
+class Product(BaseModel):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
