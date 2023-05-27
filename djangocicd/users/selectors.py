a6d7c25e16ce991e88570e64121520c9b0a2769a
Amir <amircodes10@gmail.com>
2023-05-27 23:58:22 +0330

    create get_user_profile service

--- djangocicd/users/selectors.py
+++ djangocicd/users/selectors.py
@@ -1,3 +1,11 @@
-def get_user_profile(user):
-    pass
+from django.contrib.auth import get_user_model
+
+from .models import Profile
+
+
+User = get_user_model()
+
+
+def get_user_profile(user: User) -> Profile:
+    return Profile.objects.get(user=user)
 
