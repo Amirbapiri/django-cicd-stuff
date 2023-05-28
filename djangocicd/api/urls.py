c8b5885a356987182694a6e21c99031d554c52fb
Amir <amircodes10@gmail.com>
2023-05-28 22:13:18 +0330

    add JWT authentication functionality

--- djangocicd/api/urls.py
+++ djangocicd/api/urls.py
@@ -2,5 +2,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('blog/', include(('djangocicd.blog.urls', 'blog'), namespace="blog")),
-    path('users/', include(('djangocicd.users.urls', 'users'), namespace="users"))
+    path('users/', include(('djangocicd.users.urls', 'users'), namespace="users")),
+    path('auth/', include(('djangocicd.authentication.urls', 'authentication'), namespace="authentication"))
 ]
+
