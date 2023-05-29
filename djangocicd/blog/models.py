c0a80a494e5bfdc26b415cabb9c68468af6e652c
Amir <amircodes10@gmail.com>
2023-05-29 19:41:48 +0330

    create 'Post' and 'Subscription' models

--- djangocicd/blog/models.py
+++ djangocicd/blog/models.py
@@ -1,11 +1,39 @@
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 from django.db import models
 
 from djangocicd.common.models import BaseModel
 
 
-class Product(BaseModel):
-    name = models.CharField(max_length=255)
+User = get_user_model()
+
+
+class Post(BaseModel):
+    slug = models.SlugField(primary_key=True, max_length=100)
+    title = models.CharField(max_length=100, unique=True)
+    content = models.TextField()
+    author = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="posts",
+    )
 
     def __str__(self):
-        return self.name
+        return self.slug
+
+
+class Subscription(models.Model):
+    subscriber = models.ForeignKey(User, on_delete=models.CASCADE, related_name="subs")
+    target = models.ForeignKey(User, on_delete=models.CASCADE, related_name="targets")
+
+    class Meta:
+        unique_together = ("subscriber", "target")
+
+    def clean(self):
+        if self.subscriber == self.target:
+            raise ValidationError({"subscriber": {"'subscriber' and 'target' can not be equivalent"}})
+
+    def __str__(self):
+        return f"{self.subscriber.email} - {self.target.email}"
 
